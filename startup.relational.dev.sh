0838c5277d536610d1a8de2e324c4b4057580c8e
Maksym Hnoinskyi <63585714+mhnoinskyi@users.noreply.github.com>
2025-04-16 00:18:07 +0300

    Testing init script

--- startup.relational.dev.sh
+++ startup.relational.dev.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh host.docker.internal:5432
 npm run migration:run
 npm run seed:run:relational
 npm run start:prod
--- startup.relational.test.sh
+++ startup.relational.test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-/opt/wait-for-it.sh postgres:5432
+/opt/wait-for-it.sh host.docker.internal:5432
 /opt/wait-for-it.sh maildev:1080
 npm install
 npm run migration:run
